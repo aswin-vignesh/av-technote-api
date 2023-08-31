65ebfaca9f2396768cf30fa99718a4abfb0b9e3e
aswin-vignesh <80969724+aswin-vignesh@users.noreply.github.com>
2023-08-31 17:21:34 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://av-technote.onrender.com/',
+    'https://av-technote.onrender.com',
     'https://www.av-technote.onrender.com/',
+    'https://www.av-technote.onrender.com',
 ] // these localhost are allowed to access our rest api
 
 module.exports = allowedOrigins;
